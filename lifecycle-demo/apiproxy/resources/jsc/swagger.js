9bb846510e3c89e7cb9156f93ee4cd74b086622c
rf8f76e33 <cass_obregon@msn.com>
2015-08-05 09:52:37 +0000

    Changes made..

---
+++ lifecycle-demo/apiproxy/resources/jsc/swagger.js
@@ -0,0 +1,69 @@
+{
+  "swagger": "2.0",
+  "info": {
+    "title": "lifecycle-demo",
+    "description": "",
+    "version": "1.0.0"
+  },
+  "host": "example.com",
+  "schemes": [
+    "http"
+  ],
+  "basePath": "/v1/catalogs",
+  "paths": {
+    "/address": {
+      "get": {
+        "description": "==> tbd",
+        "parameters": [
+          {
+            "name": "sample",
+            "in": "query",
+            "required": true,
+            "type": "integer",
+            "format": "int32"
+          }
+        ],
+        "responses": {
+          "200": {
+            "description": "==> tbd",
+            "schema": {
+              "$ref": "#/definitions/Response"
+            }
+          },
+          "404": {
+            "description": "==> tbd",
+            "schema": {
+              "$ref": "#/definitions/Error"
+            }
+          }
+        }
+      }
+    }
+  },
+  "definitions": {
+    "Body": {
+      "properties": {
+        "bodyParam": {
+          "description": "tbd",
+          "type": "string"
+        }
+      }
+    },
+    "Response": {
+      "properties": {
+        "responseParam": {
+          "description": "tbd",
+          "type": "string"
+        }
+      }
+    },
+    "Error": {
+      "properties": {
+        "errorParam": {
+          "description": "tbd",
+          "type": "string"
+        }
+      }
+    }
+  }
+}
